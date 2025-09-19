50c7622f8711e6e8e006255f08117cecea71a1b9
marianoluna <mariano29878@gmail.com>
2025-09-19 17:34:13 -0600

    Database functions
    termine de escribir funciones para add y get de canciones y artistas,
    respectivamente

--- database.py
+++ database.py
@@ -10,11 +10,11 @@ import pandas as pd
 
 from models import Song, Artist, get_engine
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy import select, update
+from sqlalchemy import select
 
-global engine
+global engine #, async_engine
 engine = get_engine()
-Session = sessionmaker(engine, expire_on_commit= False)
+# async_engine = create_async_engine()
 
 from contextlib import contextmanager
 
@@ -31,6 +31,13 @@ def get_session():
     finally:
         session.close()
 
+# Por el momento no conviene hacer nada en async.
+# @contextmanager
+# def get_async_session():
+#     Session = sessionmaker(async_engine, expire_on_commit= False)
+#     session = Session()
+#     pass
+
 def add_artist_data(session,
                     artist_name: str,
                     spotify_id: str = None):
@@ -45,13 +52,11 @@ def add_artist_data(session,
     if not artist:
         new_artist = Artist(name= artist_name, spotify_id= spotify_id)
         session.add(new_artist)
-        # session.commit()
     else:
         print(f"Artist found: {artist!r}")
         if not artist.spotify_id and spotify_id:
             print("Updating artist's spotify_id")
             artist.spotify_id = spotify_id
-            # session.commit()        
     return
             
 
@@ -75,13 +80,26 @@ def add_song_data(session,
                         artist= Artist,
                         spotify_id= spotify_id)
         session.add(new_song)
-        # session.commit()
         
 def get_song_database(session) -> pd.DataFrame:
     if not session:
         raise Exception("Error. No session was provided")
     
     result = session.execute(
-        select(Song.id, Song.name, Song.album, Song.artist_id)
+        select(Song.id, Song.name, Song.album, Song.artist_id, Song.spotify_id)
         ).mappings().all()
+    df = pd.DataFrame(result)
+    df['modified'] = False
+    return df
+
+def get_artist_database(session) -> pd.DataFrame:
+    if not session:
+        raise Exception("Error. No session was provided")
+    
+    result = session.execute(
+        select(Artist.id, Artist.name, Artist.spotify_id)
+        ).mappings().all()
+    df = pd.DataFrame(result)
+    df['modified'] = False
+    return df
         
